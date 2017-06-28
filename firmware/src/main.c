4e750c8e2f6783b42d29fbd46d498f79aa4dcdd7
Jon Lamb <jlamb@polysync.io>
2017-06-28 11:42:19 -0700

    Initial firmware commit, main.c, Makefile

---
+++ firmware/src/main.c
@@ -0,0 +1,20 @@
+/**
+ * @file main.c
+ * @brief Main.
+ *
+ */
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/pgmspace.h>
+
+
+int main(void)
+{
+
+    return 0;
+}
